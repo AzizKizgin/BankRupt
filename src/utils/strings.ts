e648fb7554d7f4a0abe393c6f1e602a2ad52d206
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-26 22:06:30 +0300

    feat: new strings added

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -2,4 +2,6 @@ export const strings = {
   welcome: 'WELCOME!',
   forgetPassword: 'Forget Password?',
   createAccount: 'Create Account',
+  passwordMustBeAtLeast: 'Password must be at least 8 characters',
+  agreeToTerms: 'I agree to the terms and conditions',
 };

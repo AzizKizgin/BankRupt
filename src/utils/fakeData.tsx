4a1ea5037d5fcf3b2e19c0894f0045e8fdc4011f
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-28 00:05:41 +0300

    feat: fakeData added

---
+++ src/utils/fakeData.tsx
@@ -0,0 +1,74 @@
+export const fakeData = [
+  {
+    id: 1,
+    title: 'Lorem Ipsum Company',
+    amount: '$3,251.52',
+    type: 0,
+  },
+  {
+    id: 2,
+    title: 'Dolor Sit Ltd.',
+    amount: '$4,573.72',
+    type: 1,
+  },
+  {
+    id: 3,
+    title: 'Amet Consectetur est.',
+    amount: '$1,000.00',
+    type: 2,
+  },
+  {
+    id: 4,
+    title: 'Adipiscing Elit Ltd.',
+    amount: '$2,000.00',
+    type: 3,
+  },
+  {
+    id: 5,
+    title: 'Lorem Ipsum Company',
+    amount: '$3,251.52',
+    type: 0,
+  },
+  {
+    id: 6,
+    title: 'Dolor Sit Ltd.',
+    amount: '$4,573.72',
+    type: 1,
+  },
+  {
+    id: 7,
+    title: 'Amet Consectetur est.',
+    amount: '$1,000.00',
+    type: 2,
+  },
+  {
+    id: 8,
+    title: 'Adipiscing Elit Ltd.',
+    amount: '$2,000.00',
+    type: 3,
+  },
+  {
+    id: 9,
+    title: 'Lorem Ipsum Company',
+    amount: '$3,251.52',
+    type: 0,
+  },
+  {
+    id: 10,
+    title: 'Dolor Sit Ltd.',
+    amount: '$4,573.72',
+    type: 1,
+  },
+  {
+    id: 11,
+    title: 'Amet Consectetur est.',
+    amount: '$1,000.00',
+    type: 2,
+  },
+  {
+    id: 12,
+    title: 'Adipiscing Elit Ltd.',
+    amount: '$2,000.00',
+    type: 3,
+  },
+];

46902a81b8136ec8898402d19941b5f04e8460ce
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-27 16:19:00 +0300

    feat: screen updated

--- src/screens/ForgetPassword.tsx
+++ src/screens/ForgetPassword.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
-import {Box, Text} from 'native-base';
-
+import React, {useState} from 'react';
+import {Box, Image, Input, Text, VStack} from 'native-base';
+import Button from '../components/shared/Button';
+import {strings} from '../utils/strings';
+import {useNavigation} from '@react-navigation/native';
+import {useToast} from '@aziz_kizgin/react-native-toast-message';
 const ForgetPassword = () => {
+  const [email, setEmail] = useState('');
+  const navigation = useNavigation();
+  const {showToast} = useToast();
+  const onPress = () => {
+    showToast({
+      color: '#00e21',
+      message: 'Reset link sent to your email',
+    });
+    navigation.goBack();
+  };
   return (
-    <Box>
-      <Text>ForgetPassword</Text>
+    <Box flex={1} backgroundColor="white">
+      <Box
+        alignItems="center"
+        justifyContent="center"
+        height={200}
+        bg={{
+          linearGradient: {
+            colors: ['darkBlue.900', 'darkBlue.800'],
+            start: [0, 0],
+            end: [0, 1],
+          },
+        }}>
+        <Image
+          source={require('../assets/images/resetPassword.png')}
+          alt="logo"
+          width={100}
+          height={100}
+          resizeMode="contain"
+          tintColor="white"
+        />
+      </Box>
+      <Box alignItems="center" paddingY={12}>
+        <Text
+          fontSize="lg"
+          fontWeight="bold"
+          color="darkBlue.900"
+          marginBottom={8}>
+          {strings.sendResetLink}
+        </Text>
+        <VStack space={4}>
+          <Input placeholder="Email" value={email} onChangeText={setEmail} />
+          <Button onPress={onPress} title="Send" />
+        </VStack>
+      </Box>
     </Box>
   );
 };

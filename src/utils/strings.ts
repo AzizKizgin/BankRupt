1c9ef12740c52a4741248b924d7ccbae9201462a
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-28 00:04:29 +0300

    feat: new word added

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -5,4 +5,5 @@ export const strings = {
   passwordMustBeAtLeast: 'Password must be at least 8 characters',
   agreeToTerms: 'I agree to the terms and conditions',
   sendResetLink: 'Send Password Reset Link',
+  latestTransactions: 'Latest Transactions',
 };

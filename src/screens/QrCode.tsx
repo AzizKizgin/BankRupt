bd17ec7c250db8920bbe57e480719318b8b9e441
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-28 22:45:41 +0300

    feat: screen updated

--- src/screens/QrCode.tsx
+++ src/screens/QrCode.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {Box, Text} from 'native-base';
+import {Box, Image, Text} from 'native-base';
 
 const QrCode = () => {
   return (
-    <Box>
-      <Text>QrCode</Text>
+    <Box flex={1} backgroundColor={'white'}>
+      <Box variant="screenTop" flex={1}>
+        <Text color="white" fontSize="lg" marginBottom={4}>
+          Scan QR Code
+        </Text>
+        <Image
+          source={require('../assets/images/qrCode.png')}
+          alt="QR Code"
+          width={200}
+          height={200}
+          resizeMode="contain"
+        />
+      </Box>
     </Box>
   );
 };

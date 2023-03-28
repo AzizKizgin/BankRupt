a7db2d4275a7ac09353d65cea910d826a2f754d7
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-28 03:06:52 +0300

    refactor: unused imports removed

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Text, VStack} from 'native-base';
-import {Alert, BackHandler} from 'react-native';
-import ExitAppModal from '../components/Home/ExitAppModal';
-import HomeAccountIcon from '../components/Home/HomeAccountIcon';
-import Button from '../components/shared/Button';
+import React from 'react';
+import {Box, Text} from 'native-base';
 import AccountInfo from '../components/Home/AccountInfo';
 import LatestTransactions from '../components/Home/LatestTransactions';
 

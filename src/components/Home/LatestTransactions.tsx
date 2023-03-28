5f9c9cacea072457dfd9c790185768a7e3bfe45f
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-28 03:06:35 +0300

    feat: screen updated

--- src/components/Home/LatestTransactions.tsx
+++ src/components/Home/LatestTransactions.tsx
@@ -3,11 +3,17 @@ import {Box, Center, HStack, Text} from 'native-base';
 import {fakeData} from '../../utils/fakeData';
 import {strings} from '../../utils/strings';
 import TransactionItem from '../shared/TransactionItem';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const LatestTransactions = () => {
   const transactions: Transaction[] = fakeData.slice(0, 4);
+  const navigation = useNavigation<NativeStackNavigationProp<MainParamList>>();
+  const onMorePress = () => {
+    navigation.navigate('Transaction History');
+  };
   return (
-    <Box flex={1} paddingX={6}>
+    <Box paddingX={6} flex={1}>
       <Center marginTop={4}>
         <Text fontSize="lg" fontWeight="bold" color="cyan.600">
           {strings.latestTransactions}
@@ -16,6 +22,15 @@ const LatestTransactions = () => {
       {transactions.map((transaction: Transaction, index) => (
         <TransactionItem key={index} transaction={transaction} />
       ))}
+      <Text
+        onPress={onMorePress}
+        textAlign="right"
+        color="cyan.600"
+        fontSize="sm"
+        marginTop={6}
+        alignSelf="flex-end">
+        {strings.more}
+      </Text>
     </Box>
   );
 };

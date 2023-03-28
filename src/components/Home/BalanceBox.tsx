48562a5525617a9ebaf2c34425480fbc432d8811
Aziz Kizgin <azizkzgn@gmail.com>
2023-03-28 03:06:08 +0300

    feat: onPress added

--- src/components/Home/BalanceBox.tsx
+++ src/components/Home/BalanceBox.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Box, Text} from 'native-base';
 import Button from '../shared/Button';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 const BalanceBox = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<MainParamList>>();
+  const onTransferPress = () => {
+    navigation.navigate('Transaction');
+  };
   return (
     <Box
       w={250}
@@ -17,7 +23,7 @@ const BalanceBox = () => {
       <Text fontSize="3xl" fontWeight="bold" color="darkBlue.900">
         $4,180.20
       </Text>
-      <Button onPress={() => {}} title="Transfer" />
+      <Button onPress={onTransferPress} title="Transfer" />
     </Box>
   );
 };
